fbf6c583f8f87d4e3f6af430e340c81191a6fa92
Eduardo <es9828478@gmail.com>
2026-04-27 16:35:40 -0600

    "Se instala express"

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // Para que el servidor entienda datos en formato JSON
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
+
+let pedidos = [];
+
+
+app.get('/productos', (req, res) => {
+    res.json([
+        { nombre: "Pizza",       precio: 120 },
+        { nombre: "Hamburguesa", precio: 90  },
+        { nombre: "Ensalada",    precio: 70  },
+        { nombre: "Refresco",    precio: 30  }
+    ]);
+});
+
+
+app.post('/pedido', (req, res) => {
+    const pedido = req.body;
+    pedidos.push(pedido);
+    res.json({ mensaje: "Pedido recibido", pedido });
+});
+
+
+app.get('/pedidos', (req, res) => {
+    res.json(pedidos);
+});
+
+
+app.delete('/pedido/:id', (req, res) => {
+    const id = req.params.id;
+    pedidos.splice(id, 1);
+    res.json({ mensaje: `Pedido ${id} eliminado` });
+});
+
+
+app.put('/pedido/:id', (req, res) => {
+    const id = req.params.id;
+    pedidos[id] = req.body;
+    res.json({ mensaje: `Pedido ${id} actualizado`, pedido: pedidos[id] });
+});
